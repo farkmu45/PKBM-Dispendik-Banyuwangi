96c0a874728614d610f4ec6f7fa0fa2c24efb4f6
farkmu45 <farukmicros@outlook.com>
2022-11-10 16:57:11 +0700

    Disable screen transition in activity screen

--- screens/activities/ActivityScreen.js
+++ screens/activities/ActivityScreen.js
@@ -1,19 +1,15 @@
-import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
 import { ActivityDetail, ActivityList } from '../../constants/screens'
-import ActivityListScreen from './ActivityListScreen'
 import ActivityDetailScreen from './ActivityDetailScreen'
+import ActivityListScreen from './ActivityListScreen'
 
 const Stack = createNativeStackNavigator()
 
 export default function ActivityScreen() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name={ActivityList}
-        screenOptions={{ animation: 'none' }}
-        component={ActivityListScreen}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
+      <Stack.Screen name={ActivityList} component={ActivityListScreen} />
       <Stack.Screen name={ActivityDetail} component={ActivityDetailScreen} />
     </Stack.Navigator>
   )
