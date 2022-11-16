ff6c4fd72b92c9c3a9454e6f1cc4fa52d34d34d2
farkmu45 <farukmicros@outlook.com>
2022-11-16 09:59:14 +0700

    UI tweaks

--- screens/auth/ForgotPasswordScreen.js
+++ screens/auth/ForgotPasswordScreen.js
@@ -1,10 +1,11 @@
+import * as NavigationBar from 'expo-navigation-bar'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Image, ScrollView, Text, TextInput, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Button from '../../components/Button'
+import Input from '../../components/Input'
 import colors from '../../constants/colors'
-import * as NavigationBar from 'expo-navigation-bar'
 
 export default function ForgotPasswordScreen() {
   NavigationBar.setBackgroundColorAsync(colors.primary[100])
@@ -13,16 +14,17 @@ export default function ForgotPasswordScreen() {
       <StatusBar backgroundColor='transparent' style='dark' />
       <ScrollView>
         <View className='px-5 items-center py-10'>
-          <Image source={require('../../assets/images/logo.png')} />
-          <Text className='text-2xl mt-5 font-SemiBold'>Reset Password</Text>
+          <Image
+            className='h-40'
+            resizeMode='contain'
+            source={require('../../assets/images/logo.png')}
+          />
+          <Text className='text-2xl mt-10 font-Medium'>Reset Password</Text>
           <View className='rounded-md p-5 bg-white w-full mt-6'>
             <Text className='text-base text-black mb-5 font-Regular'>
               Masukkan email yang terdaftar untuk mendapatkan sandi baru.
             </Text>
-            <TextInput
-              placeholder='E-mail'
-              className='border-b-2 border-gray-400 text-base'
-            />
+            <Input placeholder='E-mail' />
           </View>
           <Button className='self-stretch mt-5'>Kirim</Button>
         </View>
